e8bbcb05343b185d4c1a6c479ad4b9196fa6c052
Divyam <makhaikdivyam8@gmail.com>
2023-01-05 02:44:36 +0530

    update

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.use('/Quiz', userRoutes)
     res.sendFile(path.resolve(__dirname ,'./frontend' , 'build' ,'index.html'))
   })
   
+  
 // }else{
 //   module.exports = require('./src/config/dev')
 // }
